ea79b8bf75fd1cd17d47c4db3e1845767b74f103
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-02 16:09:56 +0530

    Update Header UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,6 +11,10 @@ const Header = () => {
           <img src={assets.star_icon} className='w-2.5' alt="" />
         </div>
 
+        <h1 className="text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700">Your own <span className='text-primary'>blogging </span> <br/> platform.</h1>
+
+        <p className="my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500">This is your space to think out loud, to share what matters, and to write without filter. Whether it's one word or a thousand, your story starts right here.</p>
+
       </div>
       <img src={assets.gradientBackground} alt="" className='absolute top-50 z-10 opacity-50' />
     </div>
