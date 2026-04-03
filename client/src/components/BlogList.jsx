1152fb0cb375422261a7f3573d4bd56bd358b76b
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-03 16:23:46 +0530

    Add motion animation to BlogList menu

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { blogCategories } from '../assets/assets'
+import { motion } from "motion/react"
+
 
 const BlogList = () => {
 
@@ -14,7 +16,9 @@ const BlogList = () => {
               className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
               {item}
               {menu === item && (
-                <div className="absolute left-0 right-0 top-0 h-7 -z-10 bg-primary rounded-full"></div>
+                <motion.div layoutId='underline'
+                  transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                  className="absolute left-0 right-0 top-0 h-7 -z-10 bg-primary rounded-full"></motion.div>
               )}
             </button>
           </div>
