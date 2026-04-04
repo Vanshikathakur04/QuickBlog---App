19fc94164f42d3f800978324df020e2a118c2551
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-04 16:14:52 +0530

    feat: add styled footer with copyright text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
+      <div>
+
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">Copyright 2026 © QuickBlog - All rights reserved.</p> 
     </div>
   )
 }
