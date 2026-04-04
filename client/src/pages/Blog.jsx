f55d9f9d49b2e0b2888acdbe800ccec5a839d918
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-04 17:37:44 +0530

    Fetch blog data based on route params

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { blog_data } from '../assets/assets'
 
 const Blog = () => {
-  return (
+
+  const { id } = useParams()
+
+  const [data, setData] = useState(null)
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find(item => item._id === id)
+    setData(data)
+  }
+
+  useEffect(() => {
+    fetchBlogData()
+  }, [])
+
+  return data ? (
     <div>
       <h1>Blog</h1>
     </div>
-  )
+  ) : <div>Loading...</div>
 }
 
 export default Blog
