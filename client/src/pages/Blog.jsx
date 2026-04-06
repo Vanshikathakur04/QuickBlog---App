26f84b49d5f6fb6a4063a553f248a4dbd00d9ebb
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-06 15:01:54 +0530

    add navbar and styling to blog page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { blog_data } from '../assets/assets'
+import { assets, blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
+import Moment from 'moment'
 
 const Blog = () => {
 
@@ -18,8 +20,20 @@ const Blog = () => {
   }, [])
 
   return data ? (
-    <div>
-      <h1>Blog</h1>
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute -top-50 z-i opacity-50' />
+
+      <Navbar />
+
+      <div className='text-center mt-10 text-gray-600'>
+        <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">{data.title}</h1>
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-priamry">Mikasa Ackerman</p>
+      </div>
+
+      <div></div>
+
     </div>
   ) : <div>Loading...</div>
 }
