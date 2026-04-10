0e3dc8c2cd437b04636f6ba576ee0030284442a3
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-10 16:48:20 +0530

    Add password support to login

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
   }
 
@@ -14,11 +14,16 @@ const Login = () => {
             <h1 className='text-3xl font-bold'><span className='text-primary'>Admin</span>Login</h1>
             <p className='font-light'>Enter your credentials to access the admin panel</p>
           </div>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className='mt-6 w-full sm:ma-w-md text-gray-600'>
             <div className="flex flex-col">
               <label>Email</label>
               <input type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
             </div>
+            <div className="flex flex-col">
+              <label>Password</label>
+              <input type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+            </div>
+            <button type='submit' className="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all">Login</button>
           </form>
         </div>
       </div>
