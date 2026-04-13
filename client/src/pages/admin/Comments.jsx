5ea02d9582b9dc172d0f73b0cee3857b7b169e50
Vanshikathakur04 <thakurvanshika876@gmail.com>
2026-04-13 17:41:44 +0530

    Implemented Comments component with approve filter

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { comments_data } from '../../assets/assets'
 
 const Comments = () => {
+
+  const [comments, setComments] = useState([])
+  const [filter, setFilter] = useState('Not Approved')
+
+  const fetchComments = async () => {
+    setComments(comments_data)
+  }
+
+  useEffect(() => {
+    fetchComments()
+  }, [])
+
+
   return (
-    <div>
-      
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
+      <div className="flex justify-between items-center max-w-3xl">
+        <h1>Comments</h1>
+        <div className="flex gap-4">
+          <button onClick={() => setFilter('Approved')}
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Approved' ? 'text-primary' : 'text-gray-700'}`}>Approved</button>
+
+          <button onClick={() => setFilter('Not Approved')}
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Not Approved' ? 'text-primary' : 'text-gray-700'}`}>Not Approved</button>
+        </div>
+      </div>
     </div>
   )
 }
